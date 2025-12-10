377beb138741bab7215a3622fd9ff843d1608c89
majorli <154788733@qq.com>
2025-12-10 23:58:46 +0800

    CODING: Qwen3 reranker --Libin

--- src/embedding_service/models/__init__.py
+++ src/embedding_service/models/__init__.py
@@ -2,10 +2,12 @@ from .bge_m3 import BGEM3
 from .qwen3_embedding import Qwen3Embedding
 from .splade_v3 import Splade_v3
 from .bge_reranker import BGEReranker
+from .qwen3_reranker import Qwen3Reranker
 
 __all__ = [
     "BGEM3",
     "Qwen3Embedding",
     "Splade_v3",
     "BGEReranker",
+    "Qwen3Reranker",
 ]
--- src/embedding_service/models/qwen3_reranker.py
+++ src/embedding_service/models/qwen3_reranker.py
@@ -4,11 +4,19 @@ from .abstract_models import AbstractReranker
 class Qwen3Reranker(AbstractReranker):
     _tokenizer = None
     _model = None
+    _pool = None
+    _devices = None
     _query_instruction = None
-    _passage_instruction = None
     _batch_size = None
     _max_length = None
-    _device = None
+
+    _PREFIX = (
+        "<|im_start|>system\nJudge whether the Document meets the "
+        "requirements based on the Query and the Instruct provided. "
+        "Note that the answer can only be \"yes\" or \"no\"."
+        "<|im_end|>\n<|im_start|>user\n"
+    )
+    _SUFFIX = "<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
 
     @classmethod
     def rank(
@@ -16,7 +24,6 @@ class Qwen3Reranker(AbstractReranker):
         query: str,
         passages: str | list[str],
         query_instruction: str | None = None,
-        passage_instruction: str | None = None,
         batch_size: int | None = None,
         max_length: int | None = None,
         **kwargs,
@@ -27,7 +34,8 @@ class Qwen3Reranker(AbstractReranker):
         Ensures the model is started up before reranking, otherwise returns
         an empty list.
 
-        Qwen3-Reranker models confirm to normalize the final scores.
+        Qwen3-Reranker models confirm to normalize the final scores. It does
+        not use passage instruction for reranker.
 
         Args:
             query (str):
@@ -36,8 +44,6 @@ class Qwen3Reranker(AbstractReranker):
                 A list of passage strings to be reranked.
             query_instruction: (str | None):
                 Instruction for queries.
-            passage_instruction: (str | None):
-                Instruction for passages.
             batch_size (int | None):
                 The batch size for processing passages. Default is None.
             max_length (int | None):
@@ -80,7 +86,6 @@ class Qwen3Reranker(AbstractReranker):
         model_name_or_path: str,
         device: str | None = None,
         query_instruction: str | None = None,
-        passage_instruction: str | None = None,
         batch_size: int = 128,
         **kwargs,
     )-> None:
@@ -90,10 +95,11 @@ class Qwen3Reranker(AbstractReranker):
         The batch size and instructions are set during startup and will be
         used as defaults. They can be overridden while compute scores.
 
+        Qwen3-Reranker models confirm to normalize the final scores. It does
+        not use passage instruction for reranker.
+
         Other default parameters including:
-        - normalize = True, can be overridden.
-        - max_length = 3072, fixed.
-        - use_fp16 = False, fixed.
+        - max_length = 3072
 
         Args:
             model_name_or_path (str):
@@ -102,30 +108,50 @@ class Qwen3Reranker(AbstractReranker):
                 Device to run the model on.
             query_instruction: (str | None):
                 Instruction for queries.
-            passage_instruction: (str | None):
-                Instruction for passages.
             batch_size: (int):
-                BGE-Reranker-v2-m3 default batch size, 128.
+                batch size, default is 128.
         """
         if cls._model is not None:
             return
-
         if not model_name_or_path:
             raise ValueError("model_name_or_path must be provided.")
 
-        # from FlagEmbedding import FlagReranker
-        # TODO
+        import torch
+        from transformers import (
+            AutoTokenizer,
+            AutoModelForCausalLM,
+            is_torch_npu_available,
+        )
+
+        cls._query_instruction = (
+            query_instruction or 
+            "Given the user query, retrieval the relevant passages"
+        )
+        cls._batch_size = batch_size
+        cls._max_length = 3072
+        cls._devices = cls.get_local_devices(device)
+        cls._pool = None
 
         try:
-            cls._model = FlagReranker(
-                model_name_or_path.strip(),
-                devices = device,
-                query_instruction_for_rerank = query_instruction,
-                passage_instruction_for_rerank = passage_instruction,
-                batch_size = 128,
-                normalize = True,
-                use_fp16 = False,
-                max_length = 3072,
+            cls._tokenizer = AutoTokenizer.from_pretrained(
+                model_name_or_path,
+                trust_remote_code=True,
+                padding_side='left',
+            )
+            cls._model = AutoModelForCausalLM.from_pretrained(
+                model_name_or_path,
+                trust_remote_code=True,
+                dtype=torch.float32,
+            )
+            cls.token_false_id = cls._tokenizer.convert_tokens_to_ids("no")
+            cls.token_true_id = cls._tokenizer.convert_tokens_to_ids("yes")
+            cls.prefix_tokens = cls._tokenizer.encode(
+                cls._PREFIX,
+                add_special_tokens=False,
+            )
+            cls.suffix_tokens = cls._tokenizer.encode(
+                cls._SUFFIX,
+                add_special_tokens=False,
             )
             logger.info(f"{model_name_or_path} loaded.")
         except Exception as e:
@@ -133,16 +159,35 @@ class Qwen3Reranker(AbstractReranker):
             cls._model = None
             return
 
-        try:
-            _ = cls._model.compute_score(['query', 'passage'])
-            logger.info(f"{model_name_or_path} warmed-up.")
-        except Exception as e:
-            logger.error(f"Warming-up {model_name_or_path} failed: {e}")
-            return
-
     @classmethod
     def shutdown(cls):
         if cls._model is not None:
+            cls._tokenizer = None
             cls._model = None
-            logger.info("BGE-Reranker-v2-m3 model shutdown.")
+            cls._pool = None    # TODO
+            cls._devices = None
+            cls._query_instruction = None
+            cls._batch_size = None
+            cls._max_length = None
+            logger.info("Qwen3-Reranker model shutdown.")
+
+    @staticmethod
+    def get_local_devices(device: str | None)-> list[str]:
+        import torch
+        from transformers import is_torch_npu_available
+
+        if not device:
+            if torch.cuda.is_available():
+                return [f"cuda:{i}" for i in range(torch.cuda.device_count())]
+            elif is_torch_npu_available():
+                return [f"npu:{i}" for i in range(torch.npu.device_count())]
+            elif hasattr(torch, "musa") and torch.musa.is_available():
+                return [f"musa:{i}" for i in range(torch.musa.device_count())]
+            elif torch.backends.mps.is_available():
+                return ["mps"]
+            else:
+                return ["cpu"]
+        else:
+            return [device]
+
 
